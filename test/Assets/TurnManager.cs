1e4b78563c5ac9307863faa6d42aede1129b72e1
archandy <archandy85@gmail.com>
2015-05-11 23:23:42 +0900

    Unity_1
    Earth/Moon

---
+++ test/Assets/TurnManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(0,1,0);
+		gameObject.transform.Rotate (0, 0, 0);
+	}
+}
